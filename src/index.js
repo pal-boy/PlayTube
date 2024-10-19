6999485f0afaa40532d0aba93ce3e580e49db6df
Aakash Pal <akashpal1482@gmail.com>
2024-10-19 21:47:35 +0530

    utilities added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 // require('dotenv').config({path:'./env'});
 import dotenv from 'dotenv'
 import db_connection from "./db/index.js";
+import app from './app.js';
 
 dotenv.config({
     path:'./env'
 });
-db_connection();
+db_connection()
+.then(()=>{
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`Server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Databse connection failed : ",err);
+});
 
 // import express from "express";
 // const app = express();
