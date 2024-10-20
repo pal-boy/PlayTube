cfeb196cc8438033e37fb7233962e74d82c77d63
Aakash Pal <akashpal1482@gmail.com>
2024-10-20 10:33:11 +0530

    database connected succesfully

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const db_connection = async ()=>{
     try {
-        const connnection_instance = await mongoose.connect(`${process.env.DATABASE_URL}/${DB_NAME}`);
+        const connnection_instance = await mongoose.connect("mongodb://localhost:27017/PlayTube");
         console.log(`Database connected!! DB Host: ${connnection_instance.connection.host}`);
     } catch (error) {
         console.log("Database connection error : ",error);
